cf0735facf5f7fb503ca024d237de5dfb8a51f96
hdknr <gmail@hdknr.com>
2015-03-18 16:41:15 +0000

    MailAddress

--- web/alumni/migrations/0001_initial.py
+++ web/alumni/migrations/0001_initial.py
@@ -7,44 +7,18 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('emailqueue', '0004_attachment_file'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Letter',
-            fields=[
-                ('mail_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='emailqueue.Mail')),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('emailqueue.mail',),
-        ),
-        migrations.CreateModel(
             name='Profile',
             fields=[
-                ('contact_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='emailqueue.Contact')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('family_name', models.CharField(max_length=50, verbose_name='Family Name')),
                 ('first_name', models.CharField(max_length=50, verbose_name='First Name')),
             ],
             options={
-                'abstract': False,
             },
-            bases=('emailqueue.contact',),
-        ),
-        migrations.CreateModel(
-            name='Alumnus',
-            fields=[
-                ('profile_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='alumni.Profile')),
-                ('entered_year', models.IntegerField(verbose_name='Entered Year')),
-                ('entered_school', models.CharField(max_length=50, verbose_name='Entered School')),
-                ('graduated_year', models.IntegerField(verbose_name='Entered Year')),
-                ('graduated_school', models.CharField(max_length=50, verbose_name='Entered School')),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('alumni.profile',),
+            bases=(models.Model,),
         ),
     ]
