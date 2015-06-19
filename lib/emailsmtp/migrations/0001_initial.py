2d8128249e752a29f76b893af30a229817caa87e
hdknr <gmail@hdknr.com>
2015-06-19 07:24:53 +0000

    delete migrations

--- lib/emailsmtp/migrations/0001_initial.py
+++ lib/emailsmtp/migrations/0001_initial.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('emailqueue', '0001_initial'),
     ]
 
     operations = [
@@ -28,14 +27,20 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='SmtpServer',
+            name='Server',
             fields=[
-                ('service_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='emailqueue.Service')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created At')),
+                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Updated At')),
+                ('name', models.CharField(unique=True, max_length=50, verbose_name='Mail Service Name')),
+                ('domain', models.CharField(unique=True, max_length=50, verbose_name='Mail Domain Name')),
+                ('backend', models.CharField(default=b'django.core.mail.backends.smtp.EmailBackend', max_length=100, verbose_name='Mail Backend')),
+                ('wait_every', models.IntegerField(default=0, help_text='Wait sending for every count help', verbose_name='Wait sending for every count')),
+                ('wait_ms', models.IntegerField(default=0, help_text='Wait milliseconds help', verbose_name='Wait milliseconds')),
             ],
             options={
                 'verbose_name': 'SMTP Server',
                 'verbose_name_plural': 'SMTP Server',
             },
-            bases=('emailqueue.service',),
         ),
     ]
