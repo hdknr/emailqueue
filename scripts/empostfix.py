4cadafe9d14bcd6b7733c1ea03829a60bc1df5fa
hdknr <gmail@hdknr.com>
2015-06-21 08:09:24 +0000

    empostfix.py command

--- scripts/empostfix.py
+++ scripts/empostfix.py
@@ -1,37 +1,30 @@
 '''
 empostfix manager:
 
+    argv[1] : directory path which includes "settings.py"
 
 '''
 import sys
 import os
-import imp
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.stderr.write('you need path of your django application')
         sys.exit(1)
 
-    #: django path
+    #  django path
+    APP = os.path.basename(sys.argv[1])
     sys.argv[1] = os.path.abspath(sys.argv[1])
     sys.path.append(os.path.dirname(sys.argv[1]))
-    sys.path.append(sys.argv[1])
 
-    #: argv[1] is manage.py command
+    #  argv[1] is manage.py command `empostfix`
     sys.argv[1] = 'empostfix'
 
-    try:
-        imp.find_module('settings')  # Assumed to be in the same directory.
-    except ImportError:
-        import sys
-        sys.stderr.write(str(sys.path))
-        sys.stderr.write("".join([
-            "Error: Can't find the file 'settings.py' ",
-            "in the directory containing %r. ",
-            "It appears you've customized things.\n",
-            "You'll have to run django-admin.py, ",
-            "passing it your settings module.\n"]) % __file__)
-        sys.exit(1)
+    # DJANGO_SETTINGS_MODULE
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "{0}.settings".format(APP))
 
+    # Run managemnet command
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
